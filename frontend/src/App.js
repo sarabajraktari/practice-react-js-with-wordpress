8bae6df4d831a6aeccdd82928f72aad4c8d5f2fe
Sara Bajraktari <sara.bajraktari@g2m-ext.com>
2025-07-09 15:54:47 +0200

    frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Posts from './components/pages/Posts';
+
+function App() {
+  return (
+    <>
+      <Posts />
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/pages/Posts/index.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const Posts = () => {
+    const [posts, setPosts] = useState([]);
+    useEffect(() => {
+        let url = `${process.env.REACT_APP_API_ROOT}/posts`;
+        axios.get(url).then((res) => {
+            setPosts(res.data);
+        });
+    }, []);
+    console.log('posts', posts);
+
+    return (
+        <>
+            <div className="w-4/5 py-10 m-auto flex justify-between align-middle flex-wrap gap-10">
+                {
+                    posts && posts.map(post => {
+                        return (
+                            <div key={post.id} className='card p-3 w-[330px] shadow-lg rounded-lg flex gap-5 flex-col'>
+                                <img src='https://placehold.co/400'  alt='example-img'/>
+                                <h2 className='text-lg front-bold'>{post.title.rendered}</h2>
+                                <p dangerouslySetInnerHTML={{__html: post.excerpt.rendered}}></p>
+                                {post.title.rendered}
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        </>
+    )
+}
+
+export default Posts;
